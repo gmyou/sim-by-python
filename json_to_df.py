bfe20147c6b091a154bd1de1658d1b91b74e4f0b
유근명 <gmyou@motiv-i.com>
2023-10-25 15:34:11 +0900

    refactor : 변수 처리

--- json_to_df.py
+++ json_to_df.py
@@ -1,10 +1,13 @@
 import json, pandas as pd
 # test.json 내용: [{"name":"Jack","age":26},{"name":"Ace","age":87}]
  
-with open('test.json') as f:
+source_json = 'test.json'
+output_json = 'write.json'
+
+with open(source_json) as f:
     js = json.loads(f.read()) ## json 라이브러리 이용
 df = pd.DataFrame(js)
-df = pd.read_json('test.json') ## pd.read_json 이용
+df = pd.read_json(source_json) ## pd.read_json 이용
  
 ## orient
 df.to_json() # default : orient='columns'
@@ -15,5 +18,5 @@ df.to_json(orient='index')
 # Output : '{"0":{"name":"Jack","age":26},"1":{"name":"Ace","age":87}}'
  
 ## json으로 쓰기
-df.to_json('write.json',orient='index')
+df.to_json(output_json,orient='index')
 ## -> pd.read_json()으로 json을 읽으면 orient를 자동으로 인식
